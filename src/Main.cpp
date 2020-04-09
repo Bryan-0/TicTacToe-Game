c1c2a08602c66a626bbe89288ab144756f5ed8af
Bryan AA <abrayan87@yahoo.com>
2020-04-09 12:37:38 -0600

    Add isMatchTied function.
    - Checks if no one wins.

--- src/Main.cpp
+++ src/Main.cpp
@@ -32,8 +32,8 @@ int main() {
 	int firstPlayer, secondPlayer, xWins = 0, oWins = 0;
 	
 	// Turns Logic:
-	// Every even number, Player 1 will be able to place X.
-	// Every odd number, Player 2 will be able to place O.
+	// Every even number, Player 1 will move.
+	// Every odd number, Player 2 will move.
 	int turnCounter = 0;
 	
 	// Main Game Loop:
@@ -73,6 +73,32 @@ int main() {
 					cout << "X Wins: " << xWins  <<  " ||| " << " O Wins: " << oWins << endl;
 					continue;
 				} else {
+					cout << "X Wins: " << xWins << " ||| " << " O Wins: " << oWins << endl;
+					cout << "Thanks for playing!" << endl;
+					cout << "Developed by Brayan Ayala in C++" << endl;
+					break;
+				}
+			
+			}
+			
+			// Check if match is tied
+			if (gameManager.isMatchTied(gameBoard)) {
+				cout << "Match is TIED!" << endl;
+				
+				int playAgain;
+				cout << "Do you want to play again? (1 for yes) (0 for no)" << endl;
+				cin >> playAgain;
+				
+				if (playAgain == 1) {
+					turnCounter = 0;
+					
+					// Each Game Board index = ' '
+					gameManager.resetBoard(gameBoard);
+					gameManager.showBoard(gameBoard);
+					cout << "X Wins: " << xWins  <<  " ||| " << " O Wins: " << oWins << endl;
+					continue;
+				} else {
+					cout << "X Wins: " << xWins << " ||| " << " O Wins: " << oWins << endl;
 					cout << "Thanks for playing!" << endl;
 					cout << "Developed by Brayan Ayala in C++" << endl;
 					break;
@@ -119,6 +145,31 @@ int main() {
 				}
 				
 			}
+			
+			// Check if match is tied
+			if (gameManager.isMatchTied(gameBoard)) {
+				cout << "Match is TIED!" << endl;
+				
+				int playAgain;
+				cout << "Do you want to play again? (1 for yes) (0 for no)" << endl;
+				cin >> playAgain;
+				
+				if (playAgain == 1) {
+					turnCounter = 0;
+					
+					// Each Game Board index = ' '
+					gameManager.resetBoard(gameBoard);
+					gameManager.showBoard(gameBoard);
+					cout << "X Wins: " << xWins  <<  " ||| " << " O Wins: " << oWins << endl;
+					continue;
+				} else {
+					cout << "X Wins: " << xWins << " ||| " << " O Wins: " << oWins << endl;
+					cout << "Thanks for playing!" << endl;
+					cout << "Developed by Brayan Ayala in C++" << endl;
+					break;
+				}
+			}
+			
 		}
 		
 		turnCounter += 1;
